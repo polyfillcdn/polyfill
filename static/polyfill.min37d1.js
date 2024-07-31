ab51a8a3b76fd0cf75ef76bc483c76be8a5c569e
polyfillcdn <polyfillcdn@proton.me>
2024-07-31 13:32:33 +0200

    Add files via upload

---
+++ static/polyfill.min37d1.js
@@ -0,0 +1,38 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+/* Polyfill service v3.111.0
+ * Disable minification (remove `.min` from URL path) for more info */
+
+
+
+}
+/*
+     FILE ARCHIVED ON 12:31:01 Jan 08, 2023 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 10:22:20 Jul 31, 2024.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.535
+  exclusion.robots: 0.016
+  exclusion.robots.policy: 0.007
+  esindex: 0.011
+  cdx.remote: 13.161
+  LoadShardBlock: 73.805 (3)
+  PetaboxLoader3.datanode: 125.625 (4)
+  load_resource: 113.337
+  PetaboxLoader3.resolve: 38.07
+*/
